d241b71ca894ca97b8bbe095d0a3abf3a4f35a45
Gregori Mikina <gregorimikina@gmail.com>
2021-07-01 22:38:27 +0200

    Create folder and files

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const books = [
+    {
+        id:1,
+        title:'Going',
+        author:'React Stefan',
+        year:'2011',
+    },
+    {
+        id:2,
+        title:'React Next Step',
+        author:'Go to IT',
+        year:'2004'
+    }
+];
