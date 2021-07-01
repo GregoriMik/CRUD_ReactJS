04abaa90df18d9d8b24b0b4b640cb3f913820e6e
Gregori Mikina <gregorimikina@gmail.com>
2021-07-02 00:44:16 +0200

    Update App to Class Component

--- src/containers/Books.js
+++ src/containers/Books.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { books } from '../data.js';
-
+import Book from '../components/Book';
 class Books extends Component 
 {
     // constructor () {
@@ -24,21 +24,7 @@ class Books extends Component
                         {
                             books.map(book=>{
                                 return(
-                                    <tr key={book.id}>
-                                        <td>{book.id}</td>
-                                        <td>{book.title}</td>
-                                        <td>{book.author}</td>
-                                        <td>{book.year}</td>
-                                        <td>
-                                            <button type="button" className="btn btn-danger">
-                                                Delete
-                                            </button>
-                                            <button type="button" className="btn btn-default">
-                                                Edit
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    
+                                    <Book key={book.id} book={book}/>
                                 )
                             })
                         }
